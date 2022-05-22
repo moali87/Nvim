b2ce08ee05738453061d884182dc7fe1abfc8b10
Mo Ali <akali1987@gmail.com>
2022-05-22 12:55:47 -0400

    - Fix CMP

--- lua/plugin-configs/cmp.lua
+++ lua/plugin-configs/cmp.lua
@@ -1,4 +1,8 @@
 local lspconfig = require('lspconfig')
+local luasnip = require('luasnip')
+local cmp = require('cmp')
+local capabilities = require('plugin-configs.lsp-capabilities')
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -29,8 +33,6 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
-
-local capabilities = require('plugin-configs.lsp-capabilities')
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
@@ -63,6 +65,92 @@ lspconfig.sumneko_lua.setup {
   },
 }
 
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- Cmp setup
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+       luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+     completion = cmp.config.window.bordered(),
+     documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({
+      select = true,
+      behavior = cmp.ConfirmBehavior.Replace,
+    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'fish' },
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'nvim_lua' },
+    { name = 'path' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
 local servers = {
   'clangd',
   'rust_analyzer',
@@ -79,4 +167,4 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-return lspconfig
+return cmp
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,19 +4,6 @@ return packer.startup(function ()
   use({'wbthomason/packer.nvim'})
 
   use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = function ()
-      require('nvim-treesitter.configs').setup(require('lua.plugin-configs.treesitter'))
-    end
-  })
-
-  use({'tanvirtin/monokai.nvim'})
-  use({'folke/lsp-colors.nvim'})
-  use({'nvim-lua/plenary.nvim'})
-  use({'kdheepak/lazygit.nvim'})
-
-  use({
     'lewis6991/impatient.nvim',
     config = function ()
       require('impatient').enable_profile()
@@ -24,18 +11,33 @@ return packer.startup(function ()
   })
 
   use({
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'mtoohey31/cmp-fish',
-    'saadparwaiz1/cmp_luasnip',
-    requires = {{'neovim/nvim-lspconfig'}, opt = true},
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
     config = function ()
-      require('plugin-configs.nvim-lspconfig')
+      require('nvim-treesitter.configs').setup(require('plugin-configs.treesitter'))
+    end
+  })
+
+  use({
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-emoji',
+      'f3fora/cmp-spell',
+      {'mtoohey31/cmp-fish', ft = 'fish'},
+      'saadparwaiz1/cmp_luasnip',
+      {'L3MON4D3/LuaSnip', config = function ()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end}
+    },
+    config = function ()
+      require('plugin-configs.cmp')
     end
   })
 
@@ -60,7 +62,7 @@ return packer.startup(function ()
 
   use({
     'akinsho/bufferline.nvim',
-    tag = "v2.*",
+    tag = 'v2.*',
     requires = {{'kyazdani42/nvim-web-devicons'}, opt = true}, config = function ()
       require('bufferline').setup(
         require('plugin-configs.bufferline')
@@ -76,12 +78,17 @@ return packer.startup(function ()
     end
   })
 
+  use({'tanvirtin/monokai.nvim'})
+  use({'folke/lsp-colors.nvim'})
+  use({'nvim-lua/plenary.nvim'})
+  use({'kdheepak/lazygit.nvim'})
+
   use({
-      "nvim-neorg/neorg",
-      config = function()
-        require('neorg').setup(require('plugin-configs.neorg'))
-      end,
-      requires = "nvim-lua/plenary.nvim"
+    'nvim-neorg/neorg',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('neorg').setup(require('plugin-configs.neorg'))
+    end
   })
 
   use({
@@ -119,13 +126,6 @@ return packer.startup(function ()
   })
 
   -- Lazy loaded
-  use({
-    'L3MON4D3/LuaSnip',
-    config = function ()
-      require('luasnip.loaders.from_vscode').lazy_load()
-    end
-  })
-
   use({
     'windwp/nvim-autopairs',
     requires = {{'hrsh7th/nvim-cmp'}, opt = true},
