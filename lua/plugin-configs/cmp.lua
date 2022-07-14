94178c763956e24b9dfad0f70f992f53b249bdad
Mo Ali <akali1987@gmail.com>
2022-07-14 10:27:30 -0400

    - Add golang lsp

--- lua/plugin-configs/cmp.lua
+++ lua/plugin-configs/cmp.lua
@@ -36,6 +36,26 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
+local configs = require 'lspconfig/configs'
+
+if not configs.golangcilsp then
+ 	configs.golangcilsp = {
+		default_config = {
+			cmd = {'golangci-lint-langserver'},
+			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+			init_options = {
+					command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json" };
+			}
+		};
+	}
+end
+
+lspconfig.golangci_lint_ls.setup {
+	filetypes = {'go','gomod'},
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 lspconfig.sumneko_lua.setup {
   on_attach = on_attach,
   capabilities = capabilities,
--- lua/plugin-configs/null-ls.lua
+++ lua/plugin-configs/null-ls.lua
@@ -1,13 +1,13 @@
-require("null-ls").setup({
+local null_ls = require("null-ls")
+
+null_ls.setup({
   sources = {
-    require("null-ls").builtins.diagnostics.eslint_d,
-    require("null-ls").builtins.diagnostics.golangci_lint,
-    require("null-ls").builtins.completion.luasnip,
-    require("null-ls").builtins.diagnostics.markdownlint,
-    require("null-ls").builtins.diagnostics.pylint,
-    require("null-ls").builtins.diagnostics.fish,
-    require("null-ls").builtins.formatting.stylua,
-    -- require("null-ls").builtins.completion.spell,
-    -- require("null-ls").builtins.diagnostics.luacheck,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.diagnostics.golangci_lint,
+    null_ls.builtins.completion.luasnip,
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.fish,
+    null_ls.builtins.formatting.stylua
   },
 })
