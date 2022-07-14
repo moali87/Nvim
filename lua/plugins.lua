d379d99382c1d7cba24eed702d796e7b74052142
Mo Ali <akhter.ali@upstart.com>
2022-07-14 15:51:48 -0400

    - Add lspinstaller

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -124,6 +124,23 @@ return packer.startup(function()
     end
   })
 
+  -- lspinstaller
+  use({
+    'williamboman/nvim-lsp-installer',
+    config = function ()
+      require("nvim-lsp-installer").setup({
+        automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+        ui = {
+          icons = {
+              server_installed = "✓",
+              server_pending = "➜",
+              server_uninstalled = "✗"
+          }
+        }
+    })
+    end
+  })
+
   -- lint
   use({
     "jose-elias-alvarez/null-ls.nvim",
